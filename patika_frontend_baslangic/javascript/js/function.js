c16291a81872420c207806e3bce10f5c0c2f9306
furkannibis <ibis.furkan@ogr.ahievran.edu.tr>
2023-08-08 17:54:19 +0300

    func with parameters

---
+++ patika_frontend_baslangic/javascript/js/function.js
@@ -0,0 +1,45 @@
+function hello() {
+  console.log("Hello, World!");
+  merhaba();
+}
+
+function merhaba() {
+  console.log("Merhaba, Dünya!");
+}
+
+function userCheck(username, age) {
+  let info = document.querySelector("h3#info");
+  if (!username && !age) {
+    info.innerHTML = "Kullanıcı adı bilgisi verilmemiş";
+    info.innerHTML += "<br>Kullanıcı yaş bilgisi verilmemiş";
+  } else if (!username) {
+    info.innerHTML = "Kullanıcı adı bilgisi verilmemiş";
+  } else if (!age) {
+    info.innerHTML = "Kullanıcı yaş bilgisi verilmemiş";
+  } else {
+    if (age >= 18) {
+      info.innerHTML = "Ehliyet alabilirsiniz.";
+    }
+  }
+}
+
+hello();
+userCheck("Furkan", 20);
+
+let firstname = "Furkan";
+function greetings(firstname = "", lastname = "") {
+  console.log(
+    `Merhaba, ${firstname ? firstname : "Guest"} ${
+      lastname ? lastname : "Guest"
+    }`
+  );
+}
+console.log(firstname);
+let info = greetings("", "");
+console.log(info);
+
+function greetings2(firstname = "", lastname = "") {
+  let info = `Merhaba, ${firstname ? firstname : "Guest"} ${
+    lastname ? lastname : "Guest"
+  }`;
+}
