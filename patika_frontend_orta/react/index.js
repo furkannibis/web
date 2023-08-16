358df1763fa763b3942807d9029b5121201465ce
furkannibis <ibis.furkan@ogr.ahievran.edu.tr>
2023-08-16 15:10:20 +0300

    yeni egitim

---
+++ patika_frontend_orta/react/index.js
@@ -0,0 +1,12 @@
+// import slugify from "slugify";
+
+// var slugify = require("slugify");
+// const title = slugify("Some String Lorem Ipsum.", "_");
+// console.log(title);
+
+import hello, { topla, cikart, text, user } from "./my_module.js";
+
+hello("Furkan");
+console.log(topla(1, 2));
+console.log(text);
+console.log(user.userName);
---
+++ patika_frontend_orta/react/my_module.js
@@ -0,0 +1,12 @@
+export const hello = (name) => console.log("Hello", name);
+export const topla = (a, b) => a + b;
+export const cikart = (a, b) => a - b;
+
+export const text = "Text";
+
+export const user = {
+  userName: "Furkan",
+  password: "123",
+};
+
+export default hello;
