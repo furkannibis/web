5d1b5f0455604b6a8978936540e9b8dd96f7de7a
furkannibis <ibis.furkan@ogr.ahievran.edu.tr>
2023-10-03 10:30:25 +0300

    A short introduction to javascript

--- jonas_html_css/omnifood-effect-optimizations-deployment/js/script.js
+++ jonas_html_css/omnifood-effect-optimizations-deployment/js/script.js
@@ -5,11 +5,11 @@ console.log(myName);
 const h1 = document.querySelector('.heading-primary');
 console.log(h1)
 
-h1.addEventListener('click', function() {
-  h1.textContent = myName;
-  h1.style.backgroundColor = 'red';
-  h1.style.padding = '5rem';
-})
+// h1.addEventListener('click', function() {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = 'red';
+//   h1.style.padding = '5rem';
+// })
 
 
 const yearEl = document.querySelector('.year');
